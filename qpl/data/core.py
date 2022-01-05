fb6d29a9ec4f7e8276eb9ce1913fb0b5248fc4cb
jhurley-figure <jhurley@figure.com>
2022-01-05 16:53:05 +0000

    more infrastructure

---
+++ qpl/data/core.py
@@ -0,0 +1,120 @@
+import logging
+import os
+from os.path import join
+from typing import List
+
+import bqutil.core as bq
+import dask
+import dask.dataframe as dd
+import pandas as pd
+from ray.util.dask import ray_dask_get
+
+import dmnet.config.config as cfg
+import dmnet.data.features.main as fm
+from dmnet import util
+
+logger = logging.getLogger(__name__)
+MAX_BQ_THREADS = 4
+DEFAULT_IDX_COLS = ["record_nb"]
+LABEL_COL = "population_group"
+TARGET_ROWS_EXPECTED = 1_500_000
+PROJECT = 'figure-development-data'
+DATASET = 'data_science_staging'
+EXP_TABLES = ['experian_main_2112A', 'experian_premier_2112A']
+SEED_LOC = "gs://experian-archive-bureau"
+
+
+def process_training_data() -> None:
+    """
+    Load experian training data: For each date, load a date group, and union the results.
+    """
+    #dask.config.set(scheduler=ray_dask_get)
+
+    dates = ("0215", "0515", "0815", "1115")
+    dates = ("0215", "0515")
+    
+    #gs_bucket = cfg.path["src_data_dir"]["seed"]
+    gs_bucket = SEED_LOC
+    #idx_cols = cfg.idx_cols
+    idx_cols = ['record_nb', 'campaign']
+    df = dd.concat(
+        [_merge_date_group(date=d, gs_bucket=gs_bucket, idx_cols=idx_cols) for d in dates],
+        axis=0
+    )
+
+    df = df.compute()
+    #assert df.shape[0] == TARGET_ROWS_EXPECTED
+    
+    df = trim_to_srv_cols(df)
+    df["record_nb"] = 1
+    df["record_nb"] = df["record_nb"].cumsum().astype(int)
+    df = df.sample(frac=1.0, random_state=0).reset_index(drop=True)
+
+    df = dd.from_pandas(df, npartitions=10)
+    #df.to_parquet(cfg.merged_data_path_tr, overwrite=True)
+    df.to_parquet("gs://jdh-bucket/projects/qpl/data/training.parquet", overwrite=True)
+
+
+def get_valid_cols() -> List[str]:
+    """
+    Will return a set of valid columns since the training data has a superset of the 
+    columns in the serving data
+    """
+    cols = []
+    for tbl in EXP_TABLES:
+        cols = cols + bq.get_columns(project=PROJECT, dataset=DATASET, table=tbl)
+    return set(cols)
+
+def trim_to_srv_cols(xs: pd.DataFrame) -> pd.DataFrame:
+    """
+    Does the actual paring down of columns
+    """
+    exp_cols = get_valid_cols()
+    cols = set(xs.columns).intersection(exp_cols).to_list() + ['y']
+    return xs[[cols]].copy()
+
+def _merge_date_group(
+    date: str,
+    gs_bucket: str,
+    idx_cols: List[str]
+) -> dd.DataFrame:
+    """
+    For a given date, load PremierAB, PremierCD, and SCORE, and merge them
+    on record_nb.
+    Parameters
+    ----------
+    date: Experian-type date like '0415'
+    gs_bucket: Path where experian data lives.
+    idx_cols: Index columns
+    """
+    print (date)
+    def load_experian_csv_wrapper(suffix: str) -> dd.DataFrame:
+        fname = f"A1805077_{date}_{suffix}.csv"
+        path = join(gs_bucket, fname)
+        return _load_experian_csv(path=path, idx_cols=idx_cols)
+
+    dfs_src = [load_experian_csv_wrapper(p) for p in ("PremierAB", "PremierCD", "SCORE")]
+    merged = dd.concat(dfs_src, axis=1, join="inner")
+    
+    #merged[cfg.target_col] = (merged[LABEL_COL] == "PL").astype(int)
+    merged['y'] = (merged[LABEL_COL] == "PL").astype(int)
+    merged = merged.drop(LABEL_COL, axis=1)
+    merged = merged.reset_index(drop=True)
+    return merged
+
+def _load_experian_csv(path, idx_cols: List[str]) -> dd.DataFrame:
+    df = dd.read_csv(path, delimiter="|", sample_rows=100_000, assume_missing=True)
+    df.columns = df.columns.str.lower()
+    legal_prefix = ("premier", "fic")
+    feature_cols = [c for c in df.columns if c.startswith(legal_prefix)]
+    bk_cols = idx_cols + [LABEL_COL]
+    keep_cols = feature_cols + bk_cols
+    keep_cols = df.columns.intersection(keep_cols).to_list()
+    df = df[keep_cols]
+    df["record_nb"] = df["record_nb"].astype(int)
+    df = df.set_index("record_nb")
+    return 
+
+if __name__ == "__main__":
+    get_valid_cols()
+#    process_training_data()
--- qpl/model/core.py
+++ qpl/model/core.py
@@ -1,42 +1,131 @@
+import os
+from typing import Any, Dict, List, Sequence, Tuple, Type, Union
+from subprocess import Popen
+
+from hyperopt import hp, fmin, tpe, Trials, space_eval, STATUS_OK
 import pandas as pd
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import roc_auc_score, average_precision_score, mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
+import xgboost as xgb
 
-import fund_net.data.core as fndc
-import fund_net.model.core as fndm
+import cac_calibrate.binner as b
+import dmnet.util as utils
+import dmnet.data.features.prune as prn
+import bqutil.core as bq
 
-if __name__=="__main__":
-    df = pd.read_parquet("gs://jdh-bucket/projects/fund_net/data/initial_extract")
+PROJ_DIR = "gs://jdh-bucket/projects/qpl/"
 
-    mim = fndc.ModelInputMgr(design_matrix = df, target_col="app_amount")
-    desmat_dict = mim.tr_te_val_dict
+class ClassificationModelMgr(object):
+    def __init__(self,
+            data_dict: Dict,
+            target_col: str,
+            opt_hyper: bool = False
+    ):
+        self.target_col = target_col
+        self.data_dict = data_dict
+        self.Xtr = self.data_dict['tr'].drop(self.target_col,axis=1)
+        self.ytr = self.data_dict['tr'][self.target_col]
+        if 'vl' in self.data_dict.keys():
+            self.Xvl = self.data_dict['vl'].drop(self.target_col, axis=1)
+            self.yvl = self.data_dict['vl'][self.target_col]
+            self.use_cv = False
+        else:
+            self.use_cv = True
+       
+        if opt_hyper:
+            param_star = self.optimize_hyperparams()
+            print (param_star)
+            self.train_clf(hyperparams = param_star['reg_params'])
+        else:
+            self.train_clf(hyperparams=xgb_default_params())
 
-    USE_LOG = False
+    def hyperparams(self):
+        params = dict()
+        params['reg_params'] = xgb_param_space()
+        params['fit_params'] = xgb_fit_parms()
+        params['loss'] = lambda y, pred: roc_auc_score(y, pred)
+        return params
 
-    data_clnr = fndc.DataCleaner(
-            data_dict = desmat_dict,
-            target_col = "app_amount",
-            log_y = USE_LOG,
-            max_corr = 0.8,
-            bin_y = False 
-    )
+    def tune_clf(self, params: Dict[str, Any]):
+        clf = xgb.XGBClassifier(**params['reg_params'])
+        clf.fit(self.Xtr, self.ytr, eval_set=[(self.Xtr, self.ytr)], **params['fit_params'])
+        if self.use_cv:
+            cv_loss = cross_val_score(estimator=clf, X=self.Xtr, y=self.ytr, cv=5)
+            loss = -1 * cv_loss.mean()
+        else:
+            pred = clf.predict(self.Xvl)
+            loss = params['loss'](self.yvl, pred)
+        return {'loss': loss, 'status': STATUS_OK}
 
-    clean_desmat = data_clnr.clean_data_dict()
+    def optimize_hyperparams(self):
+        params = self.hyperparams()
+        trials = Trials()
+        fn = self.tune_clf
+        result = fmin(fn=fn, space=params, algo=tpe.suggest, max_evals=50, trials=trials)
+        best_params = space_eval(params, result)
+        return best_params
 
-    """
-    mdl_mgr = fndm.ClassificationModelMgr(
-            data_dict = clean_desmat,
-            target_col = 'app_amount',
-            model_type = 'gbm',
-            opt_hyper = False
-    )
-    """
+    def train_clf(self, hyperparams: Dict) -> None:
+        clf = xgb.XGBClassifier(**hyperparams)
+        clf.fit(self.data_dict['tr'].drop(self.target_col, axis=1), self.data_dict['tr'][self.target_col])
+        utils.pickle_dump(clf, f"gs://jdh-bucket/projects/fund_net/models/{self.model_type}_clf")
+        fi = pd.Series(clf.feature_importances_, index=self.data_dict['tr'].drop(self.target_col, axis=1).columns)
+        fi.to_csv(f'/home/josephhurley/projects/fund_net/fund_net/fi_{self.model_type}_clf.csv')
 
-    mdl_mgr = fndm.RegressionModelMgr(
-            data_dict = clean_desmat,
-            target_col = 'app_amount', 
-            model_type = 'gbm',
-            opt_hyper = True,
-            use_log = USE_LOG
-    )
+    def predict_clf(self, X: pd.DataFrame) -> pd.Series:
+        clf = utils.pickle_load(f"gs://jdh-bucket/projects/fund_net/models/{self.model_type}_clf")
+        y_hat = pd.Series(clf.predict(X), index=X.index)
+        return y_hat
 
-    res = mdl_mgr.score_models(export_res=True)
-    print (res)
+    def true_and_preds(self, Xy: pd.DataFrame) -> pd.DataFrame:
+        y_hat = self.predict_clf(Xy.drop(self.target_col, axis=1))
+        y_true = Xy[self.target_col]
+        true_and_preds = pd.concat([y_hat, y_true], axis=1).rename(columns={0: 'y_hat'})
+        return true_and_preds
+
+    def score_models(self, export_res:bool = True) -> Dict[str, Tuple]:
+        scores = {}
+        data_res = []
+        for desmat in self.data_dict.keys():
+            act_n_pred = self.true_and_preds(self.data_dict[desmat]).copy()
+            act_n_pred['type'] = desmat
+            data_res.append(act_n_pred)
+            roc = roc_auc_score(act_n_pred[self.target_col], act_n_pred['y_hat'])
+            avp = average_precision_score(act_n_pred[self.target_col], act_n_pred['y_hat'])
+            scores[desmat] = (roc, avp)
+        if export_res:
+            export_path = os.path.join(PROJ_DIR, 
+            pd.concat(data_res).to_csv(f'/home/josephhurley/projects/fund_net/fund_net/results_{self.model_type}_clf.csv')
+        return scores
+
+def xgb_param_space():
+    return {'learning_rate': hp.choice('learning_rate', np.arange(0.05, 0.3, .05)),
+            'max_depth': hp.choice('max_depth', np.arange(2, 10, 1)),
+            'min_child_weight': hp.choice('min_child_weight', np.arange(1, 8, 1)),
+            'colsample_bytree': hp.uniform('colsample_bytree', 0.25, 1.0),
+            'subsample': hp.uniform('subsample', 0.3, 0.8),
+            'max_leaves': hp.choice('max_leaves', np.arange(2, 128, 2)),
+            'gamma': hp.choice('gamma', np.arange(0, 10, 1)),
+            'n_estimators': 500
+            }
+
+def xgb_fit_parms():
+    return {
+            'eval_metric': 'mape',
+            'early_stopping_rounds': 50
+            }
+
+def xgb_default_params():
+    return {'learning_rate':0.15,
+            'max_depth': 2, #5,
+            'max_leaves': 42, #6,
+            'min_child_weight': 5, #2,
+            'gamma': 0, #2,
+            'colsample_bytree': 0.79637, #0.6244,
+            'subsample': 0.43719 #.78
+            }
+
